e42808665dc1d7974e0b09155749d1ddee0ba830
Lukas <115922955+LukasMei@users.noreply.github.com>
2023-12-11 13:41:52 +0100

    Aufgabe 5

--- MTVideo/src/bmp_io/bmp_io.java
+++ MTVideo/src/bmp_io/bmp_io.java
@@ -41,13 +41,8 @@ public final class bmp_io {
 
 			// Implementierung
 
-			//printRGB(bmp);
-
-			//bufferWith(outFilename, bmp);
-			//bufferHeight(outFilename, bmp);
-			//reduceBits(bmp,6);
-			doSomething(bmp);
-
+			convertYCbCrtoRGB(bmp);
+			
 
 			// Zugriff auf Pixel mit bmp.image.getRgbPixel(x, y);
 			// Setzen eines Pixels mit bmp.image.setRgbPixel(x, y, pc);
@@ -79,7 +74,7 @@ public final class bmp_io {
 		}
 	}
 
-	public static void printRGB(BmpImage bmp) {		
+	public static void printRGB(BmpImage bmp) {
 		for (int i = 0; i < bmp.image.getWidth(); i++) {
 			for (int j = 0; j < bmp.image.getHeight(); j++) {
 				PixelColor pOut = bmp.image.getRgbPixel(i, j);
@@ -100,7 +95,7 @@ public final class bmp_io {
 			for (int i = 0; i < bmp.image.getWidth(); i++) {
 				PixelColor pOut = bmp.image.getRgbPixel(i, 0);
 				String printRGB = "X/Y: " + i + "/0 " + "RGB: " + "(" + String.valueOf(pOut.r)
-							+ "/" + String.valueOf(pOut.g) + "/" + String.valueOf(pOut.b) + ")";
+						+ "/" + String.valueOf(pOut.g) + "/" + String.valueOf(pOut.b) + ")";
 				bw.write(printRGB);
 				bw.newLine();
 
@@ -125,7 +120,7 @@ public final class bmp_io {
 			for (int j = 0; j < bmp.image.getHeight(); j++) {
 				PixelColor pOut = bmp.image.getRgbPixel(0, j);
 				String printRGB = "X/Y: " + j + "/0 " + "RGB: " + "(" + String.valueOf(pOut.r)
-							+ "/" + String.valueOf(pOut.g) + "/" + String.valueOf(pOut.b) + ")";
+						+ "/" + String.valueOf(pOut.g) + "/" + String.valueOf(pOut.b) + ")";
 				bw.write(printRGB);
 				bw.newLine();
 			}
@@ -140,64 +135,120 @@ public final class bmp_io {
 
 	}
 
-	public static void reduceBits(BmpImage bmp,int rBits){
+	public static void reduceBits(BmpImage bmp, int rBits) {
 
 		double doubleExp = Math.pow(2, rBits);
 		int intExp = (int) doubleExp;
 
-		for(int i = 0; i < bmp.image.getWidth(); i++){
-			for(int j = 0; j < bmp.image.getHeight(); j++){
+		for (int i = 0; i < bmp.image.getWidth(); i++) {
+			for (int j = 0; j < bmp.image.getHeight(); j++) {
 
 				PixelColor pixel = bmp.image.getRgbPixel(i, j);
 
 				pixel.r = (int) (pixel.r / intExp * doubleExp);
 				pixel.g = (int) (pixel.g / intExp * doubleExp);
 				pixel.b = (int) (pixel.b / intExp * doubleExp);
-				
-				bmp.image.setRgbPixel(i, j, pixel);		
-				
+
+				bmp.image.setRgbPixel(i, j, pixel);
+
 			}
 		}
 	}
 
-	public static void doSomething (BmpImage bmp){
+	public static void convertRGBtoYCbCr(BmpImage bmp) {
 
-		double[][] matrix = {{0.299,0.587,0.114 },
-							 {-0.169,-0.331,0.5},
-							 {0.5,-0.419,-0.081}};
+		double[][] matrix = { { 0.299, 0.587, 0.114 },
+				{ -0.169, -0.331, 0.5 },
+				{ 0.5, -0.419, -0.081 } };
 
-		BmpImage yImage = new BmpImage();		
-		RgbImage rgb = new RgbImage(bmp.image.getWidth(),
-									bmp.image.getHeight(),
-									bmp.image.getBitsPerPixel());
-
-
-
-
-		for(int width = 0; width < bmp.image.getWidth(); width++){
-			for(int height = 0; height < bmp.image.getHeight(); height++ ){
+		for (int width = 0; width < bmp.image.getWidth(); width++) {
+			for (int height = 0; height < bmp.image.getHeight(); height++) {
 
 				PixelColor pixel = bmp.image.getRgbPixel(width, height);
 
-				int yValue = (int)(((matrix[0][0] * pixel.r) + 0) + 
-							((matrix[0][1] * pixel.g) + 128) + 
-							((matrix[0][2] * pixel.b) + 128));
-				int Cb = (int)(((matrix[1][0] * pixel.r) + 0) + 
-							((matrix[1][1] * pixel.g) + 128) + 
-							((matrix[1][2] * pixel.b) + 128));
-				int Cr = (int)(((matrix[2][0] * pixel.r) + 0) + 
-							((matrix[2][1] * pixel.g) + 128) + 
-							((matrix[2][2] * pixel.b) + 128));
+				int yValue = (int) (((matrix[0][0] * pixel.r) + 0) +
+						((matrix[0][1] * pixel.g) + 0) +
+						((matrix[0][2] * pixel.b) + 0));
 
-				PixelColor yPixel = new PixelColor(yValue,yValue,yValue);
-				yImage.image.setRgbPixel(width,height,yPixel);
-												
-				
+				int cb_r = (int) ((matrix[1][0] * pixel.r) + 128);
+				int cb_g = (int) ((matrix[1][1] * pixel.g) + 128);
+				int cb_b = (int) ((matrix[1][2] * pixel.b) + 128);
+
+				int cr_r = (int) ((matrix[2][0] * pixel.r) + 128);
+				int cr_g = (int) ((matrix[2][1] * pixel.g) + 128);
+				int cr_b = (int) ((matrix[2][2] * pixel.b) + 128);
+
+				PixelColor yPixel = new PixelColor(cb_r, cb_g, cb_b);
+				bmp.image.setRgbPixel(width, height, yPixel);
+
 			}
 		}
 
-		
 	}
 
-	
+	public static void convertYCbCrtoRGB(BmpImage bmp) {
+
+		double[][] matrixRGB = { { 0.299, 0.587, 0.114 },
+				{ -0.169, -0.331, 0.5 },
+				{ 0.5, -0.419, -0.081 } };
+
+		double[][] matrixYCrCb = { { 1.00, 0.00, 1.403 },
+				{ 1.00, -0.344, -0.714 },
+				{ 1.00, 1.773, 0.00 } };
+
+		for (int width = 0; width < bmp.image.getWidth(); width++) {
+			for (int height = 0; height < bmp.image.getHeight(); height++) {
+
+				PixelColor pixel = bmp.image.getRgbPixel(width, height);
+
+				int yValue = (int) (((matrixRGB[0][0] * pixel.r) + 0) +
+						((matrixRGB[0][1] * pixel.g) + 0) +
+						((matrixRGB[0][2] * pixel.b) + 0));
+
+				int cb = (int) ((matrixRGB[1][0] * pixel.r)+
+						(matrixRGB[1][1] * pixel.g)+
+						(matrixRGB[1][2] * pixel.b)) + 128;
+
+				int cr = (int) ((matrixRGB[2][0] * pixel.r) +
+						(matrixRGB[2][1] * pixel.g) +
+						(matrixRGB[2][2] * pixel.b)) + 128;
+
+				int r = (int) ((matrixYCrCb[0][0] * yValue) +
+						(matrixYCrCb[0][1] * (cb - 128)) +
+						(matrixYCrCb[0][2]) * (cr - 128));
+
+				int g = (int) ((matrixYCrCb[1][0] * yValue) +
+						(matrixYCrCb[1][1] * (cb - 128)) +
+						(matrixYCrCb[1][2]) * (cr - 128));
+
+				int b = (int) ((matrixYCrCb[2][0] * yValue) +
+						(matrixYCrCb[2][1] * (cb - 128)) +
+						(matrixYCrCb[2][2]) * (cr - 128));
+
+				PixelColor yPixel = new PixelColor(r, g, b);
+				bmp.image.setRgbPixel(width, height, yPixel);
+
+			}
+		}
+
+	}
+
+	public static void bufferHistogramm(String outFilename, BmpImage image){
+			try {
+			FileOutputStream fos = new FileOutputStream(outFilename + "_histogram.txt");
+			OutputStreamWriter osw = new OutputStreamWriter(fos, "US-ascii");
+			BufferedWriter bw = new BufferedWriter(osw);
+
+			
+			
+
+			bw.close();
+			osw.close();
+			fos.close();
+
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
+
 }
